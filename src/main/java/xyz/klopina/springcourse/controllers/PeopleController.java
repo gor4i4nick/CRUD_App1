54c3e47a930850c5e8430bad504862cb813a4fb0
Mykola Radchenko <klopina@i.ua>
2021-04-25 10:29:36 +0300

    вывел список 4х пиплов в цикле с помощью таймлив

--- src/main/java/xyz/klopina/springcourse/controllers/PeopleController.java
+++ src/main/java/xyz/klopina/springcourse/controllers/PeopleController.java
@@ -1,9 +1,7 @@
 package xyz.klopina.springcourse.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
